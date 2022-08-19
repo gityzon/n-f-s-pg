5c2ad3697f5331da74adf61bc2212acb6c4c879d
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-19 11:08:52 +0800

    Update synapse-install.sh

--- synapse-install.sh
+++ synapse-install.sh
@@ -1,11 +1,3 @@
-#安装synapse
-mkdir -p ~/${REPL_SLUG}\/synapse
-virtualenv -p python3 ~/${REPL_SLUG}\/synapse/env
-source ~/${REPL_SLUG}\/synapse/env/bin/activate
-pip install --upgrade pip
-pip install --upgrade setuptools
-pip install matrix-synapse
-
 #！生成homeserver配置文件，请更改your-domain.com!
 cd ~/${REPL_SLUG}\/synapse
 python -m synapse.app.homeserver \
